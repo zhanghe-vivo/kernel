19ee54e3d6cfa63d9c183096219b7dcdb3198b61
张河(72203484) <72203484@vivo.com>
2025-07-04 09:00:50 +0000

    import vivo license header
    Description: import vivo license header
    
    Reason: import vivo license header
    
    Issue: NA

--- infra/src/list/typed_ilist.rs
+++ infra/src/list/typed_ilist.rs
@@ -1,3 +1,17 @@
+// Copyright (c) 2025 vivo Mobile Communication Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // We are not using Pin APIs here since Pin APIs are unergonomic and
 // hard to learn for ordinary developers. We are using a smart pointer
 // to wrap a value, it's conventional that the value is pinned. This
