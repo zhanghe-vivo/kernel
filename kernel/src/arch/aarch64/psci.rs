ac5fab7142f954aa75fb9374185611f6a45af22e
11154271 <11154271@vivo.com>
2025-07-06 14:44:57 +0800

    feat: change /// to //
    Description: change /// to //
    
    Reason: change /// to //
    
    Issue: NA

--- kernel/src/arch/aarch64/psci.rs
+++ kernel/src/arch/aarch64/psci.rs
@@ -27,7 +27,7 @@ pub enum PsciFuncName {
     SystemReset = 9,
 }
 
-/// Return the version of PSCI implemented
+// Return the version of PSCI implemented
 pub fn get_psci_version(psci_base: u32) -> usize {
     let func_id = psci_base + (PsciFuncName::Version as u32);
     unsafe {
@@ -38,15 +38,15 @@ pub fn get_psci_version(psci_base: u32) -> usize {
     }
 }
 
-/// Suspend execution on a core or higher-level topology node.
-///
-/// # Arguments
-///
-/// * `power_state` - The power state.
-/// * `entry` - The entry poin.
-/// * `context_id` - The context id, this value must be present in X0
-/// *  Entry and context_id is only valid if the target power state is a powerdown state.
-/// *  For standby states, the value passed in is ignored.
+// Suspend execution on a core or higher-level topology node.
+//
+// # Arguments
+//
+// * `power_state` - The power state.
+// * `entry` - The entry poin.
+// * `context_id` - The context id, this value must be present in X0
+// *  Entry and context_id is only valid if the target power state is a powerdown state.
+// *  For standby states, the value passed in is ignored.
 pub fn cpu_suspend(psci_base: u32, power_state: usize, entry: usize, context_id: usize) {
     let func_id = psci_base + (PsciFuncName::CpuSuspend as u32);
     unsafe {
@@ -62,22 +62,22 @@ pub fn cpu_off(psci_base: u32) {
     }
 }
 
-/// Power up a core. This call is used to power up cores that eithe:
-///     1.Have not yet been booted into the calling supervisory software.
-///     2.Have been previously powered down with a CPU_OFF call.
-///
-/// # Arguments
-///
-/// * `target_cpu` - This parameter contains a copy of the affinity fields of the MPIDR registe.
-///     If the calling Exception level is using AArch64, the format is:
-///        - Bits[40:63]: Must be zero
-///        - Bits[32:39] Aff3: Match Aff3 of target core MPIDR
-///        - Bits[24:31] Must be zero
-///        - Bits[16:23] Aff2: Match Aff2 of target core MPIDR
-///        - Bits[8:15] Aff1: Match Aff1 of target core MPIDR
-///        - Bits[0:7] Aff0: Match Aff0 of target core MPIDR
-/// * `entry` - The entry poin.
-/// * `context_id` - The context id, this value must be present in X0
+// Power up a core. This call is used to power up cores that eithe:
+//     1.Have not yet been booted into the calling supervisory software.
+//     2.Have been previously powered down with a CPU_OFF call.
+//
+// # Arguments
+//
+// * `target_cpu` - This parameter contains a copy of the affinity fields of the MPIDR registe.
+//     If the calling Exception level is using AArch64, the format is:
+//        - Bits[40:63]: Must be zero
+//        - Bits[32:39] Aff3: Match Aff3 of target core MPIDR
+//        - Bits[24:31] Must be zero
+//        - Bits[16:23] Aff2: Match Aff2 of target core MPIDR
+//        - Bits[8:15] Aff1: Match Aff1 of target core MPIDR
+//        - Bits[0:7] Aff0: Match Aff0 of target core MPIDR
+// * `entry` - The entry poin.
+// * `context_id` - The context id, this value must be present in X0
 pub fn cpu_on(psci_base: u32, target_cpu: usize, entry: usize, context_id: usize) {
     let func_id = psci_base + (PsciFuncName::CpuOn as u32);
     unsafe {
@@ -94,12 +94,12 @@ pub fn cpu_on(psci_base: u32, target_cpu: usize, entry: usize, context_id: usize
     }
 }
 
-/// Enable the caller to request status of an affinity instance
-///
-/// # Arguments
-///
-/// * `target_affinity` - This follows the same format as the target_cpu parameter of a CPU_ON.
-/// * `lowest_affinity_level` - Denotes the lowest affinity level field that is valid in the target_affinity parameter.
+// Enable the caller to request status of an affinity instance
+//
+// # Arguments
+//
+// * `target_affinity` - This follows the same format as the target_cpu parameter of a CPU_ON.
+// * `lowest_affinity_level` - Denotes the lowest affinity level field that is valid in the target_affinity parameter.
 pub fn affinity_info(
     psci_base: u32,
     target_affinity: usize,
@@ -123,11 +123,11 @@ pub fn affinity_info(
     }
 }
 
-/// This is used to ask a uniprocessor Trusted OS to migrate its context to a specific core
-///
-/// # Arguments
-///
-/// * `target_cpu` - This parameter contains a copy of the affinity fields of the MPIDR registe.
+// This is used to ask a uniprocessor Trusted OS to migrate its context to a specific core
+//
+// # Arguments
+//
+// * `target_cpu` - This parameter contains a copy of the affinity fields of the MPIDR registe.
 pub fn migrate(psci_base: u32, target_cpu: usize) {
     let func_id = psci_base + (PsciFuncName::Migrate as u32);
     unsafe {
@@ -135,7 +135,7 @@ pub fn migrate(psci_base: u32, target_cpu: usize) {
     }
 }
 
-/// This function allows a caller to identify the level of multicore support present in the Trusted OS.
+// This function allows a caller to identify the level of multicore support present in the Trusted OS.
 pub fn migrate_info_type(psci_base: u32) -> usize {
     let func_id = psci_base + (PsciFuncName::MigrateInfoType as u32);
     unsafe {
@@ -146,7 +146,7 @@ pub fn migrate_info_type(psci_base: u32) -> usize {
     }
 }
 
-/// For a uniprocessor Trusted OS, this function returns the current resident core.
+// For a uniprocessor Trusted OS, this function returns the current resident core.
 pub fn migrate_info_up_cpu(psci_base: u32) -> usize {
     let func_id = psci_base + (PsciFuncName::MigrateInfoUpCpu as u32);
     unsafe {
@@ -157,7 +157,7 @@ pub fn migrate_info_up_cpu(psci_base: u32) -> usize {
     }
 }
 
-/// Shut down the system.
+// Shut down the system.
 pub fn system_off(psci_base: u32) -> ! {
     let func_id = psci_base + (PsciFuncName::SystemOff as u32);
     unsafe {
@@ -166,7 +166,7 @@ pub fn system_off(psci_base: u32) -> ! {
     core::unreachable!()
 }
 
-/// Reset the system
+// Reset the system
 pub fn system_reset(psci_base: u32) -> ! {
     let func_id = psci_base + (PsciFuncName::SystemReset as u32);
     unsafe {
