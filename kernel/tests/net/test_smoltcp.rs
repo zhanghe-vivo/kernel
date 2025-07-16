20c8c4171bb2d179aeeecb495bf0c052ab8ea7ac
陈蕴洲 <yunzhou.chen@vivo.com>
2025-07-16 05:30:58 +0000

    update smoltcp test file lisense header
    update smoltcp test file lisense header
    
    Description: update smoltcp test file lisense header
    
    Reason: license
    
    Issue: https://gitblueos.vivo.xyz/BlueOS/Kernel/BlueKernel/manifests/-/issues/94
    
    Change-Id: I4bf9106a34308e8ba256b1d3f0be514d61d9b79c

--- kernel/tests/net/test_smoltcp.rs
+++ kernel/tests/net/test_smoltcp.rs
@@ -11,11 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-//
-// This code is based on smoltcp (original license follows):
-// https://github.com/smoltcp-rs/smoltcp/blob/main/LICENSE-0BSD.txt
-// standard BSD Zero Clause License
+
+// This code is based on https://github.com/smoltcp-rs/smoltcp/blob/main/examples/loopback.rs:
+// Copyright (C) smoltcp contributors
+// SPDX-LICENSE: 0BSD
 
 use super::net_utils;
 use alloc::{boxed::Box, vec, vec::Vec};
